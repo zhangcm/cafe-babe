99999f4ae6755c0e00761f0f3147c317dcd6b263
zhangcm <zhangchaomin@qianmi.com>
2018-07-26 09:40:53 +0800

    类加载器

--- basics-demo/src/main/java/com/justz/classloader/MyDelegateClassLoader.java
+++ basics-demo/src/main/java/com/justz/classloader/MyDelegateClassLoader.java
@@ -4,18 +4,18 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * 自定义classloader
+ * 委派父类加载的自定义classloader
  *   1. 继承ClassLoader
- *   2. 复写loadClass方法（自己加载，不委托父类）
+ *   2. 复写findClass方法（先委托父类加载，父类未加载，自己再加载）
  *      2.1 获取class文件的InputStream
  *      2.2 调用父类的defineClass方法
  * @author zhangcm
  * @since 1.0, 2018/7/26 上午8:53
  */
-public class MyClassLoader extends ClassLoader {
+public class MyDelegateClassLoader extends ClassLoader {
 
     @Override
-    public Class<?> loadClass(String name) throws ClassNotFoundException {
+    public Class<?> findClass(String name) throws ClassNotFoundException {
         try {
             String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
             InputStream is = getClass().getResourceAsStream(fileName);
@@ -31,10 +31,10 @@ public class MyClassLoader extends ClassLoader {
     }
 
     public static void main(String[] args) throws Exception {
-        ClassLoader classLoader = new MyClassLoader();
+        ClassLoader classLoader = new MyDelegateClassLoader();
 
-        Object obj = classLoader.loadClass("com.justz.classloader.MyClassLoader").newInstance();
+        Object obj = classLoader.loadClass("com.justz.classloader.MyDelegateClassLoader").newInstance();
         System.out.println(obj.getClass());
-        System.out.println(obj instanceof MyClassLoader);
+        System.out.println(obj instanceof MyDelegateClassLoader);
     }
 }
