f54f15bba690471b1f248ee37491ed33ab1b3bbf
zhangcm <justif_z@163.com>
2018-10-09 12:41:25 +0800

    文件读写

--- basics-demo/src/main/java/com/justz/io/MappedByteBufferDemo.java
+++ basics-demo/src/main/java/com/justz/io/MappedByteBufferDemo.java
@@ -2,24 +2,41 @@ package com.justz.io;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class MappedByteBufferDemo {
 
+    public static void readAndWrite() throws IOException {
+        String filePath = FileCopy.class.getClassLoader().getResource("demo.txt").getFile();
+        RandomAccessFile file = new RandomAccessFile(filePath, "rw");
+        FileChannel fc = file.getChannel();
+        MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_WRITE, 30, fc.size() - 30);
+        byte[] ds = new byte[50];
+        // 索引会移动
+        mbb.get(ds, 0, 50);
+        System.out.println(new String(ds));
+        // 原位置替换，会直接修改文件，索引会后移
+        mbb.put("tings ".getBytes());
+        mbb.get(ds, 0, 50);
+        System.out.println(new String(ds));
+    }
+
     public static void readFile() throws IOException {
         String filePath = FileCopy.class.getClassLoader().getResource("flatmap.txt").getFile();
         FileInputStream fis = new FileInputStream(filePath);
         FileChannel fc = fis.getChannel();
-        byte[] ds = new byte[50];
-        // FileInputStream只能读不能写
-        MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_ONLY, 30, 50).load();
+        // FileInputStream只能读不能写, size不能超过剩余长度
+        MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_ONLY, 1950, fc.size() - 1950);
+        byte[] ds = new byte[mbb.capacity()];
         mbb.get(ds);
         String content = new String(ds);
         System.out.println(content);
     }
 
     public static void main(String[] args) throws IOException {
-        readFile();
+//        readFile();
+        readAndWrite();
     }
 }
--- basics-demo/src/main/java/com/justz/io/RandomAccessFileDemo.java
+++ basics-demo/src/main/java/com/justz/io/RandomAccessFileDemo.java
@@ -2,19 +2,36 @@ package com.justz.io;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.charset.Charset;
 
 public class RandomAccessFileDemo {
 
-    public static void readFile() throws IOException {
-        String filePath = FileCopy.class.getClassLoader().getResource("flatmap.txt").getFile();
+    public static void operate() throws IOException {
+        String filePath = FileCopy.class.getClassLoader().getResource("demo.txt").getFile();
         RandomAccessFile rcf = new RandomAccessFile(filePath, "rw");
-        rcf.seek(4);
-
+        // 调用writeChar()、writeInt()等方法会往文件里写入字节信息，直接打开文件会显示乱码
+        rcf.writeChar('a');
+        rcf.writeInt(32);
+        rcf.seek(0);
         System.out.println(rcf.readChar());
+        System.out.println(rcf.readInt());
+        rcf.close();
+    }
+
+    public static void operateString() throws IOException {
+        String filePath = FileCopy.class.getClassLoader().getResource("demo.txt").getFile();
+        RandomAccessFile rcf = new RandomAccessFile(filePath, "rw");
+        rcf.write("123".getBytes());
+        rcf.write("你好".getBytes());
+        rcf.seek(0);
+        // readLine的返回值默认ISO-8859-1编码，需转换
+        System.out.println(new String(rcf.readLine().getBytes(Charset.forName("ISO-8859-1"))));
+        rcf.close();
     }
 
     public static void main(String[] args) throws IOException {
-        readFile();
+//        operate();
+        operateString();
     }
 
 }
