750de1881c0ff6d306119d9279ddbf7ce2b889aa
zhangcm <justif_z@163.com>
2018-11-03 15:14:33 +0800

    condition

---
+++ basics-demo/src/main/java/com/justz/thread/ConditionExample.java
@@ -0,0 +1,42 @@
+package com.justz.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConditionExample {
+
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    private void before() {
+        lock.lock();
+        try {
+            System.out.println("before");
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private void after() {
+        lock.lock();
+        try {
+            condition.await();
+            System.out.println("after");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        ConditionExample example = new ConditionExample();
+        executorService.execute(example::after);
+        executorService.execute(example::before);
+    }
+}
